89e9f6901be0ca7b0100a6432e16ce466c4678d3
School <school@Mateo.attlocal.net>
2018-12-10 02:42:59 -0500

    LPTHW

---
+++ howISeeIt1.7.py
@@ -0,0 +1,69 @@
+####### 7. # More Printing
+###### 6. ## Strings and Text
+##### 5. ### More Variables and Printing
+#### 4. #### Variables and Names
+### 3. ##### Numbers and Math
+## 2. ###### Comments and Pound Characters
+# 1. ####### A Good First Program
+
+
+
+## + ## = ####
+ #
+ # print "LPTHW - mrhdman"
+### print "I decided to make a project using python."
+ # print "I'm going to challenge myself though."
+### print "I can only use stuff I learned from ex1-ex7."
+ # print "And I will only use one line of code."
+ #
+## + ## = ####
+
+
+
+# comment = "#"
+# print "What's the point of a %s?" % comment
+
+
+
+## + ## = ####
+#######
+# ex = 7
+# print "This is a review of the first", ex, "exercises in LPTHW"
+#######
+## + ## = ####
+
+
+
+## + ## = ####
+#######
+# coffee = "Sure."
+# tea = "Why not?"
+#
+# question = "Is this considered one line of code? %s %s" % (coffee, tea)
+# print question
+#######
+## + ## = ####
+
+
+
+## + ## = ####
+#######
+# lol = "That's hilarious! "
+# urlocation = "Where are you now?"
+# besafe = "I'm driving now. I will get back in touch with you soon."
+# print lol + urlocation
+# print besafe
+#######
+## + ## = ####
+
+print "One line of code?"
+
+
+
+####### 7. # More Printing
+###### 6. ## Strings and Text
+##### 5. ### More Variables and Printing
+#### 4. #### Variables and Names
+### 3. ##### Numbers and Math
+## 2. ###### Comments and Pound Characters
+# 1. ####### A Good First Program
